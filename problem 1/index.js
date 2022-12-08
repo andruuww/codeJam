e5e1ee8648a3691538a72b9d2398173677c876e8
andruuww <0427andrewnguyen@gmail.com>
2022-12-07 21:52:57 -0800

    finished problem one

--- problem 1/index.js
+++ problem 1/index.js
@@ -1,33 +1,35 @@
-let inputs = [];
-
 const readline = require('readline');
-// const { addAbortSignal } = require('stream');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
   terminal: false
 });
 
+let inputs = [];
 rl.on('line', (line) => {
-    inputs.push(Number(line.split(' ')[0]), Number(line.split(' ')[1]));
+    inputs.push([Number(line.split(' ')[0]), Number(line.split(' ')[1])]);
 });
 
 rl.on('close', () => {
     inputs.shift();
-
+    
     for (let i = 0; i < inputs.length; i++) {
-        let rows = inputs[i][0];
-        let columns = inputs[i][1];
+        console.log('Case #' + (i+1) + ':');
 
-        for (let r = 0; r < rows; r++) {
-            for (let c = 0; c < columns; c++) {
-                if (r < 2 && c < 2) {
-                    process.stdout.write('.');
+        for (let r = 0; r < inputs[i][0] * 2 + 1; r++) {
+            let lineOut = "";
+            for (let c = 0; c < inputs[i][1] + 1; c++) {
+                if (r < 2 && c < 1) {
+                    lineOut += "..";
                 } else {
-                    process.stdout.write('+');
+                    if (r%2 == 0) {
+                        lineOut += "+-"
+                    } else {
+                        lineOut += "|."
+                    }
                 }
             }
-            console.log();
+            console.log(lineOut.slice(0, -1));
         }
     }
 });
