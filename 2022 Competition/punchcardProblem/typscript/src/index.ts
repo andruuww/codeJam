769a61b08ef6b3c5215f7463f8f87fb7c905093a
nguyen andrew <47578262+andruuww@users.noreply.github.com>
2022-12-13 18:43:50 +0000

    added typescript

--- 2022 Competition/punchcardProblem/typscript/src/index.ts
+++ 2022 Competition/punchcardProblem/typscript/src/index.ts
@@ -1,1 +1,21 @@
-console.log('hello');
+import readline from 'readline';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+});
+
+let inputs:number[][];
+
+async function main(): Promise<void> {
+
+}
+
+async function readInput(): Promise<> {
+
+}
+
+rl.on('line', function(line:string): void {
+    inputs.push([Number(line.split(' ')[0]), Number(line.split(' ')[1])]);
+});
