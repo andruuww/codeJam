4bfb65d04fb0f97b0561dd1bbd6ba7b24b3aa211
nguyen andrew <47578262+andruuww@users.noreply.github.com>
2022-12-13 21:16:28 +0000

    got ts to work with promises to read line :D

--- 2022 Competition/punchcardProblem/typscript/dist/index.js
+++ 2022 Competition/punchcardProblem/typscript/dist/index.js
@@ -1,32 +1,18 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const readline_1 = __importDefault(require("readline"));
-const rl = readline_1.default.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    terminal: false
-});
-let inputs;
-function main() {
-    return __awaiter(this, void 0, void 0, function* () {
+"R
+async function readInput() {
+    return new Promise((resolve) => {
+        // create an empty string to store the numbers
+        let inputs = [];
+        rl.on('line', (line) => {
+            // split each line into an array, cast each element into a number, then push
+            inputs.push(line.split(' ').map((i) => {
+                return Number(i);
+            }));
+        });
+        // when the reader is done, resolve the promise and return values
+        rl.on('close', () => {
+            resolve(inputs);
+        });
     });
 }
-function readInput() {
-    return __awaiter(this, void 0, void 0, function* () {
-    });
-}
-rl.on('line', function (line) {
-    inputs.push([Number(line.split(' ')[0]), Number(line.split(' ')[1])]);
-});
+main();
