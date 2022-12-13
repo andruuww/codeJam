c45006ac01ee97d0e680dd32757161c15a894e81
nguyen andrew <47578262+andruuww@users.noreply.github.com>
2022-12-13 21:18:51 +0000

    fixed github :(

--- 2022 Competition/punchcardProblem/typscript/dist/index.js
+++ 2022 Competition/punchcardProblem/typscript/dist/index.js
@@ -1,4 +1,20 @@
-"R
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const readline_1 = __importDefault(require("readline"));
+const rl = readline_1.default.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+});
+async function main() {
+    let inputs = await readInput();
+    for (let i of inputs) {
+        console.log(i);
+    }
+}
 async function readInput() {
     return new Promise((resolve) => {
         // create an empty string to store the numbers
--- 2022 Competition/punchcardProblem/typscript/src/index.ts
+++ 2022 Competition/punchcardProblem/typscript/src/index.ts
@@ -6,16 +6,29 @@ const rl = readline.createInterface({
     terminal: false
 });
 
-let inputs:number[][];
-
 async function main(): Promise<void> {
+    let inputs: number[][] = await readInput();
 
+    for (let i of inputs) {
+        console.log(i);
+    }
 }
 
-async function readInput(): Promise<> {
-
+async function readInput(): Promise<number[][]> {
+    return new Promise<number[][]>((resolve) => {
+        // create an empty string to store the numbers
+        let inputs: number[][] = [];
+        rl.on('line', (line:string): void => {
+            // split each line into an array, cast each element into a number, then push
+            inputs.push(line.split(' ').map((i): number => {
+                return Number(i);
+            }));
+        });
+        // when the reader is done, resolve the promise and return values
+        rl.on('close', (): void => {
+            resolve(inputs);
+        });
+    });
 }
 
-rl.on('line', function(line:string): void {
-    inputs.push([Number(line.split(' ')[0]), Number(line.split(' ')[1])]);
-});
+main();
