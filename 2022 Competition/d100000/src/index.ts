0dd5992f8cdc82b4a218bb6c9e41c40cff4e8b61
nguyen andrew <47578262+andruuww@users.noreply.github.com>
2022-12-16 17:49:33 +0000

    started on dice problem

---
+++ 2022 Competition/d100000/src/index.ts
@@ -0,0 +1,81 @@
+import readline from 'readline';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+});
+
+async function main(): Promise<void> {
+    let input: number[][] = (await readInput()).map(function(x) {
+        return quicksort(x);
+    });
+    
+    for (let c: number = 0; c < input.length; c++) {
+        process.stdout.write(`Case #${c+1}: `);
+
+        for (let i: number = 0; i < input[c].length; i++) {
+            process.stdout.write(`${input[c][i]} `)
+        }
+
+        console.log();
+    }
+}
+
+function readInput(): Promise<number[][]> {
+    return new Promise<number[][]>((resolve) => {
+        let i = 0;
+        let input:number[][] = [];
+        rl.on('line', (line) => {
+            if (i !== 0 && i%2 === 0) {
+                input.push([0, ...line.split(' ').map((x) => {
+                    return Number(x);
+                })]);
+            }
+            i++;
+        });
+        rl.on('close', () => {
+            resolve(input);
+        });
+    });
+}
+
+function quicksort(array: number[]): number[] {
+    if (array.length <= 1) {
+        return array;
+    }
+
+    var pivot = array[0];
+    
+    var left = []; 
+    var right = [];
+
+    for (var i = 1; i < array.length; i++) {
+        array[i] < pivot ? left.push(array[i]) : right.push(array[i]);  
+    }
+
+    return quicksort(left).concat(pivot, quicksort(right));
+};
+
+function quicksortOpenAI(array: number[]): number[] {
+    if (array.length <= 1) {
+      return array;
+    }
+  
+    const pivot = array[0];
+    const left = [];
+    const right = [];
+  
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < pivot) {
+            left.push(array[i]);
+        } else {
+            right.push(array[i]);
+        } 
+    }
+  
+    return [...quicksort(left), pivot, ...quicksort(right)];
+}
+
+
+main();
